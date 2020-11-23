712a7b65ccf2aae2a3c9c4b1094fdab2f8ce9ece
kmsabbir <kmsabbirkhan225@gmail.com>
2020-11-24 01:26:13 +0600

    Updated

---
+++ Tree.java
@@ -0,0 +1,29 @@
+package com.tree;
+
+import java.util.Scanner;
+
+public class Tree {
+
+    static Node createTree(){
+    Node root =null;
+    System.out.println("Enter data:");
+    int data=sc.nextInt();
+    if(data == -1) return null;
+    root=new Node(data);
+    
+        System.out.println("Enter left data:"+data);
+        root.left=createTree();
+        
+        System.out.println("Enter right data:"+data);
+        root.right=createTree();
+    
+    return root;
+        
+    }
+   static Scanner sc=null;
+    public static void main(String[] args) {
+    sc=new Scanner(System.in);
+        createTree();
+    }
+    
+}
